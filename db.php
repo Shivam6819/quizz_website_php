8309248f692752a759d89df300801508eed7b164
Shivam6819 <ishivamverma07@gmail.com>
2025-11-04 23:12:55 +0530

    Shivam Verma Added : db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "1872003";
+$pass = "";
 $dbname = "quiz_db";
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 if (!$conn) { die("Connection failed: " . mysqli_connect_error()); }
